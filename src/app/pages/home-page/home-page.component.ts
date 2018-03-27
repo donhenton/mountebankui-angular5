5a722f8750c459c5d820d8e9edfdeb008b6a4826
Don Henton <donhenton@gmail.com>
2018-03-27 10:53:30 -0500

    match criteria 1 and documentation

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -25,6 +25,7 @@ export class HomePageComponent implements OnInit {
   currentImposterIdx;
   currentResponseIdx = 0;
   bsModalRef: BsModalRef;
+  matchTypes = ['matches', 'equals', 'contains', 'not equals', 'not contains'];
 
   constructor(private modalService: BsModalService, private impostersService: ImpostersService, private fb: FormBuilder) {
 
@@ -43,7 +44,6 @@ export class HomePageComponent implements OnInit {
       .valueChanges
       .subscribe(this.changeCollection.bind(this));
 
-    // console.log(JSON.stringify(this.currentCollection.imposters[0]));
   }
 
   /**
@@ -55,10 +55,19 @@ export class HomePageComponent implements OnInit {
     this.currentResponseIdx = 0;
   }
 
+  /**
+   * the routine used by the html (input) entries
+   * to update localstorage with each input event
+   * @param item
+   * @param event
+   */
   inputChange(item, event) {
     this.save();
   }
 
+  /**
+   * call the imposter service save and write to local storage
+   */
   save() {
     this.impostersService.save();
   }
@@ -76,6 +85,10 @@ export class HomePageComponent implements OnInit {
     this.currentCollection = this.impostersService.getCurrentImposter();
   }
 
+  /**
+   * compose display string for the imposter selection buttons
+   * @param idx
+   */
   composeImposterAlias(idx) {
     const imposter = this.currentCollection.imposters[idx];
     let verb = imposter.match.verb;
@@ -85,6 +98,9 @@ export class HomePageComponent implements OnInit {
     return 'Item ' + (idx + 1) + ' (' + verb + ')';
   }
 
+  /**
+   * add an imposter to the collection
+   */
   addImposter() {
     const newImposter = this.impostersService.createNewImposter();
     this.currentCollection.imposters.push(newImposter);
@@ -92,7 +108,9 @@ export class HomePageComponent implements OnInit {
   }
 
 
-
+  /**
+   * remove an imposter from the collection
+   */
   deleteImposter() {
     const doDelete = confirm('Delete this Imposter?');
     if (doDelete) {
@@ -104,16 +122,28 @@ export class HomePageComponent implements OnInit {
 
   }
 
+  /**
+   * used by ngFor for indexing loops
+   * @param index
+   * @param item
+   */
   trackByFn(index, item) {
     return index;
   }
 
+  /**
+   * possible callback for response/match criteria tab selection
+   * @param type
+   */
   tabSelect(type) {
     // console.log(type);
   }
 
-  ////////////// fill these in ///////////////////////
-
+  /**
+   * determine if a string is valid json
+   * @param data
+   * @returns true if valid json
+   */
   isJsonString(data) {
 
     try {
@@ -123,6 +153,11 @@ export class HomePageComponent implements OnInit {
     }
     return true;
   }
+  /**
+   * formats json
+   * @param type which area to format responseBody or body section of match
+   * @param pretty true if you want pretty printing
+   */
   formatJson(type: string, pretty: boolean) {
     let ref = null;
     if (type === 'responseBody') {
@@ -143,6 +178,10 @@ export class HomePageComponent implements OnInit {
     this.save();
   }
 
+  /**
+   * format the decorate section which is javascript as a string
+   * @param currentResponse
+   */
   formatDecorate(currentResponse) {
     if (!currentResponse.decorate) {
       currentResponse.decorate = '';
@@ -152,6 +191,11 @@ export class HomePageComponent implements OnInit {
     this.save();
   }
 
+  /**
+   * display the various help dialogs
+   *
+   * @param type
+   */
   doHelpDisplay(type) {
 
     const initialState = {};
@@ -170,7 +214,7 @@ export class HomePageComponent implements OnInit {
   /**
    * called when swapping to injection for the current response section
    * @returns {undefined}
-  */
+   */
   swapInjectionForResponse() {
 
     if (this.currentCollection.imposters[this.currentImposterIdx].responses[this.currentResponseIdx].injection.use) {
@@ -188,7 +232,6 @@ export class HomePageComponent implements OnInit {
    * @returns {undefined}
    */
   swapInjectionForMatch() {
-
     if (this.currentCollection.imposters[this.currentImposterIdx].match.injection.use) {
       const responseCopy = JSON.parse(JSON.stringify(this.currentCollection.imposters[this.currentImposterIdx].responses));
       this.currentCollection.imposters[this.currentImposterIdx] =
@@ -198,13 +241,18 @@ export class HomePageComponent implements OnInit {
     }
     this.save();
   }
-
+  /**
+    * format the inection section which is javascript as a string
+    * @param injectionSourceParent the pointer to which section has the text to format
+    */
   formatInjection(injectionSourceParent) {
     injectionSourceParent.body = js_beautify(injectionSourceParent.body);
     this.save();
   }
 
-  /// reponse CRUD
+  /**
+   * delete a response
+   */
   deleteResponse() {
     const doDelete = confirm('Delete this Response?');
     if (doDelete) {
@@ -214,7 +262,9 @@ export class HomePageComponent implements OnInit {
     }
     this.save();
   }
-
+  /**
+   * add a response
+   */
   addResponse() {
     this.currentCollection.imposters[this.currentImposterIdx]
       .responses.push(
@@ -234,6 +284,10 @@ export class HomePageComponent implements OnInit {
 
   ////////// sorting /////////////////////////////
 
+  /**
+   * compose a label for the individual imposters in the sorting dialog
+   * @param idx
+   */
   composeSortAlias(idx) {
     const imposter = this.currentCollection.imposters[idx];
     let verb = imposter.match.verb;
@@ -250,23 +304,25 @@ export class HomePageComponent implements OnInit {
    * display the sorting dialog
    *
    */
-
-
   sortImposters() {
     const me = this;
     const sortItems =
-    me.currentCollection.imposters.map((data, idx) => {
-      return { 'id': idx, 'ref': data, 'text': me.composeSortAlias(idx) };
-    });
+      me.currentCollection.imposters.map((data, idx) => {
+        return { 'id': idx, 'ref': data, 'text': me.composeSortAlias(idx) };
+      });
 
 
     const initialState: ModalOptions = {};
     initialState.class = 'main-sort-dialog';
     initialState.initialState = { sortItems: sortItems, containerRef: this };
-    this.bsModalRef = this.modalService.show(SortDialogComponent, initialState );
+    this.bsModalRef = this.modalService.show(SortDialogComponent, initialState);
 
   }
-
+  /**
+   * call back for the sort dialog
+   * @param type ok/cancel
+   * @param sortResult the sorted array
+   */
   public processSort(type, sortResult) {
     const me = this;
     if (type === 'ok') {
