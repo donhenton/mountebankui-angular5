afc1c98b4db4f57ca396da5b9ecfc57d051c9250
Don Henton <donhenton@gmail.com>
2018-03-27 13:28:49 -0500

    sort dialog test working

--- src/app/pages/home-page/sort-dialog/sort-dialog.component.spec.ts
+++ src/app/pages/home-page/sort-dialog/sort-dialog.component.spec.ts
@@ -1,7 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { SortableModule  } from 'ngx-bootstrap';
+import { SortableModule, BsModalRef  } from 'ngx-bootstrap';
 import { SortDialogComponent } from './sort-dialog.component';
-/*
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 describe('SortDialogComponent', () => {
   let component: SortDialogComponent;
   let fixture: ComponentFixture<SortDialogComponent>;
@@ -9,7 +10,9 @@ describe('SortDialogComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [SortableModule.forRoot()],
-      declarations: [ SortDialogComponent ]
+      declarations: [ SortDialogComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [BsModalRef]
     })
     .compileComponents();
   }));
@@ -20,8 +23,8 @@ describe('SortDialogComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  fit('should create', () => {
     expect(component).toBeTruthy();
   });
 });
-*/
+
